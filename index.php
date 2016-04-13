a17a04eb42942445882fad318907c10139890486
eman <amtalrahman98@gmail.com>
2016-04-13 13:57:30 +0200

    add index shimaa task

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"/>
+<style>
+#t{
+float:left;
+width:20%;
+mergin:50px;
+
+	
+}
+#s{
+	
+float:left;	
+width:20;	
+}
+.footer{clear:both
+;}
+
+
+</style>
+
+
+</head>
+<body>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>welcome to school</h1> 
+  </div>
+  <div class="row">
+    <div class="col-sm-6" id="t">
+      <h3>teacher</h3>
+      <a href="" >ادخال المدرسين</a><br/>
+      <a href="" >عرض المدرسين</a>
+    </div>
+    <div class="col-sm-6" id="s">
+      <h3>student</h3>
+       <a href="" >ادخال الطلاب</a><br/>
+      <a href="" >عرض الطلاب</a>
+    </div>
+  </div>
+</div>
+<div class="footer">
+
+&copy; school 2016
+
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
